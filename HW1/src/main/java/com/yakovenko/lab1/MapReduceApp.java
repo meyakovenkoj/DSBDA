c784fefbfddcb73152e1c7de4e2177535e103d41
Ivan Yakovenko <32681800+meyakovenkoj@users.noreply.github.com>
2022-03-23 10:23:19 +0300

    remove deprecated deps and distributed cache

--- HW1/src/main/java/com/yakovenko/lab1/MapReduceApp.java
+++ HW1/src/main/java/com/yakovenko/lab1/MapReduceApp.java
@@ -1,26 +1,25 @@
 package com.yakovenko.lab1;
 
-import lombok.extern.log4j.Log4j;
-import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.SequenceFile.CompressionType;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.compress.SnappyCodec;
 import org.apache.hadoop.mapreduce.Counter;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.util.Tool;
-import org.apache.hadoop.util.ToolRunner;
-import org.apache.hadoop.conf.Configured;
-import org.apache.hadoop.filecache.DistributedCache;
 // import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
-import org.apache.hadoop.io.compress.SnappyCodec;
-import org.apache.hadoop.io.SequenceFile.CompressionType;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
 
-@Log4j
+
 public class MapReduceApp extends Configured implements Tool
 {
+    private static final org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(MapReduceApp.class);
+    
     public static void main( String[] args ) throws Exception
     {
         int exitCode = ToolRunner.run(new MapReduceApp(), args);
@@ -32,7 +31,7 @@ public class MapReduceApp extends Configured implements Tool
             throw new RuntimeException("You should specify input, output, sectors dict and temp dict");
         }
 
-        Job job = new Job();
+        Job job = Job.getInstance();
         job.setJarByClass(MapReduceApp.class);
         job.setJobName("Click Temperature Map");
         job.setMapperClass(LabMapper.class);
@@ -46,10 +45,10 @@ public class MapReduceApp extends Configured implements Tool
         SequenceFileOutputFormat.setOutputCompressionType(job, CompressionType.BLOCK);
 
         // add coordinates - sectors dictionary
-        DistributedCache.addCacheFile(new Path(args[2]).toUri(), job.getConfiguration());
+        job.addCacheFile(new Path(args[2]).toUri());
         
         // add clicks - temperature dictionary
-        DistributedCache.addCacheFile(new Path(args[3]).toUri(), job.getConfiguration());
+        job.addCacheFile(new Path(args[3]).toUri());
 
         Path outputDirectory = new Path(args[1]);
         FileInputFormat.addInputPath(job, new Path(args[0]));
