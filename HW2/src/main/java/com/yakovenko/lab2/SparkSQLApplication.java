6686729fec3ffdc15abf16b9ddcffe538c59634f
Ivan Yakovenko <32681800+meyakovenkoj@users.noreply.github.com>
2022-04-13 04:20:11 +0300

    add java project template

---
+++ HW2/src/main/java/com/yakovenko/lab2/SparkSQLApplication.java
@@ -0,0 +1,18 @@
+package com.yakovenko.lab2;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Encoders;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+@Slf4j
+public class SparkSQLApplication {
+
+    /**
+     * @param args - args[0]: входной файл, args[1] - выходная папка
+     */
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ HW2/src/test/java/com/yakovenko/lab2/SparkTest.java
@@ -0,0 +1,17 @@
+package com.yakovenko.lab2;
+
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.Encoders;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SparkTest {
+
+    @Test
+    public void testOneArray() {
+    }
+}
