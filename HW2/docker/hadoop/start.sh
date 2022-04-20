2cb62ef0f5afff9c135f3fa1e492fe5b55b05727
Ivan Yakovenko <32681800+meyakovenkoj@users.noreply.github.com>
2022-04-21 00:41:58 +0300

    start lab

--- HW2/docker/hadoop/start.sh
+++ HW2/docker/hadoop/start.sh
@@ -7,14 +7,14 @@ sqoop import \
     --query "select array_to_string(val,',','*') from inputs where \$CONDITIONS" -m 1
 
 
-# spark-submit --class $1 \
-#     --master local --deploy-mode client \
-#     --executor-memory 1g --name wordcount \
-#     --conf "spark.app.id=$2" \
-#     /root/target/$3 \
-#     hdfs://127.0.0.1:9000/user/root/inputs/ out
+spark-submit --class $1 \
+    --master local --deploy-mode client \
+    --executor-memory 1g --name wordcount \
+    --conf "spark.app.id=$2" \
+    /root/target/$3 \
+    hdfs://127.0.0.1:9000/user/root/inputs/ out
 
-# echo "DONE! RESULT IS: "
-# hdfs fs -cat  hdfs://127.0.0.1:9000/user/root/out/part-00000
+echo "DONE! RESULT IS: "
+hdfs fs -cat  hdfs://127.0.0.1:9000/user/root/out/part-00000
 
-# hdfs fs -get /user/root/out/part-00000 /root/output/part-00000
+hdfs fs -get /user/root/out/part-00000 /root/output/part-00000
