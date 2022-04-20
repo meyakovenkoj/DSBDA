781d6c734928a169be3c4b8aa9454700b4ff50c6
Ivan Yakovenko <32681800+meyakovenkoj@users.noreply.github.com>
2022-04-21 00:40:49 +0300

    template for task

---
+++ HW2/src/main/java/com/yakovenko/lab2/ProcessCounter.java
@@ -0,0 +1,14 @@
+package com.yakovenko.lab2;
+
+import java.math.BigInteger;
+
+import org.apache.spark.sql.Dataset;
+
+import org.apache.spark.sql.Row;
+
+
+public class ProcessCounter {
+    public static Dataset<Row> process(Dataset<Row> inputDataset) {
+        return inputDataset;
+    }
+}
--- HW2/src/main/java/com/yakovenko/lab2/SparkSQLApplication.java
+++ HW2/src/main/java/com/yakovenko/lab2/SparkSQLApplication.java
@@ -1,18 +1,70 @@
 package com.yakovenko.lab2;
 
-import lombok.extern.slf4j.Slf4j;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
-@Slf4j
-public class SparkSQLApplication {
 
-    /**
-     * @param args - args[0]: входной файл, args[1] - выходная папка
-     */
+
+public class SparkSQLApplication {
+    private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(SparkSQLApplication.class);
+
     public static void main(String[] args) {
-        log.info("===========RESULT=========== ");
+        if (args.length < 2) {
+            throw new RuntimeException("Usage: java -jar SparkSQLApplication.jar input.file outputDirectory");
+        }
+        String inputDir = args[0];
+        String outputDir = args[1];
+        log.info("Appliction started!");
+        log.debug("Application started");
+        SparkSession sc = SparkSession
+                .builder()
+                .master("local")
+                .appName("SparkSQLApplication")
+                .getOrCreate();
+        FileSystem fileSystem = null;
+        String hdfsURL = "hdfs://127.0.0.1:9000/";
+        try {
+            fileSystem = FileSystem.get(new URI(hdfsURL), sc.sparkContext().hadoopConfiguration());
+
+        Path outputDirectory = new Path(hdfsURL + outputDir);
+        if (fileSystem.exists(outputDirectory)) {
+            log.info("=== Deleting output directory ===");
+            fileSystem.delete(outputDirectory, true);
+        }
+        } catch (IOException | URISyntaxException e) {
+            e.printStackTrace();
+            return;
+        }
+        Dataset<Row> df = sc.read().csv(hdfsURL + inputDir);
+        log.info("===============COUNTING...================");
+        final long startTime = System.currentTimeMillis();
+        log.info("========== Print Schema ============");
+        df.printSchema();
+        for (String col : df.columns()) {
+            df = df.withColumn(
+                col,
+                df.col(col).cast("Long")
+            );
+        }
+
+        log.info("========== Print Data ==============");
+        df.printSchema();
+        df.show();
+        Dataset<Row> result = ProcessCounter.process(df);
+        result.show();
+        final long endTime = System.currentTimeMillis();
+        log.info("============SAVING FILE TO " + hdfsURL + outputDir + " directory============");
+        log.info("Total execution time: " + (endTime - startTime));
+        result.toDF(result.columns())
+                .write()
+                .option("header", true)
+                .csv(hdfsURL + outputDir);
     }
 }
