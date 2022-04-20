5e9c2b5b9f72feeeae537fa83569a245c0abd83a
Ivan Yakovenko <32681800+meyakovenkoj@users.noreply.github.com>
2022-04-21 01:43:34 +0300

    count factorial and sum

--- HW2/src/main/java/com/yakovenko/lab2/ProcessCounter.java
+++ HW2/src/main/java/com/yakovenko/lab2/ProcessCounter.java
@@ -1,14 +1,23 @@
 package com.yakovenko.lab2;
 
 import java.math.BigInteger;
-
 import org.apache.spark.sql.Dataset;
-
 import org.apache.spark.sql.Row;
-
+import static org.apache.spark.sql.functions.*;
 
 public class ProcessCounter {
     public static Dataset<Row> process(Dataset<Row> inputDataset) {
-        return inputDataset;
+        Dataset<Row> result = inputDataset;
+        for (String col : result.columns()) {
+            result = result.withColumn(
+                col,
+                factorial(result.col(col))
+            );
+        }
+
+        result.show();
+        
+        result = result.groupBy().sum();
+        return result;
     }
 }
