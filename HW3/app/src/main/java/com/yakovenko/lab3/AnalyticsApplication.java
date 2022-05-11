341346e5e57ffd974c3dc59546f22f2a638d417d
Ivan Yakovenko <32681800+meyakovenkoj@users.noreply.github.com>
2022-05-11 03:12:56 +0300

    remove old files

--- HW3/app/src/main/java/com/yakovenko/lab3/AnalyticsApplication.java
+++
@@ -1,9 +0,0 @@
-package com.yakovenko.lab3;
-
-import io.micronaut.runtime.Micronaut;
-
-public class AnalyticsApplication {
-    public static void main(String[] args) {
-        Micronaut.run(AnalyticsApplication.class);
-    }
-}
